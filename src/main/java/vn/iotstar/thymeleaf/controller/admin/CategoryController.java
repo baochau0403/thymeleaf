6459c3ed2679ace660df93c06f149043dec02f4a
Bao Chau <138131020+baochau0403@users.noreply.github.com>
2024-11-02 16:52:00 +0700

    thêm xóa sửa

--- src/main/java/vn/iotstar/thymeleaf/controller/admin/CategoryController.java
+++ src/main/java/vn/iotstar/thymeleaf/controller/admin/CategoryController.java
@@ -2,18 +2,26 @@ package vn.iotstar.thymeleaf.controller.admin;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.validation.Valid;
@@ -29,26 +37,29 @@ public class CategoryController {
 	CategoryService categoryService;
 	
 	@RequestMapping("")
-	public String all(Model model) {
+	public String list(ModelMap model) {
 		List<CategoryEntity> list = categoryService.findAll();
-		model.addAttribute("list", list);
+		model.addAttribute("categories", list);
 		return "admin/categories/list";
 	}
+	
 	
 	@GetMapping("/add")
 	public String add(Model model) {
 		CategoryModel category = new CategoryModel();
 		category.setIsEdit(false);
 		model.addAttribute("category", category);
-		return "admin/categories/add";
+		return "admin/categories/AddOrEdit";
 	}
 	
-	@PostMapping("/save")
-	public ModelAndView saveorupdate(ModelMap model, 
-			@Valid @ModelAttribute("category") CategoryModel cateModel,BindingResult result)
-	{
+
+	
+	@PostMapping("saveOrUpdate")
+	public ModelAndView saveOrUpdate(ModelMap model, @Valid @ModelAttribute("cate") CategoryModel cateModel,
+			BindingResult result) {
+
 		if (result.hasErrors()) {
-			return new ModelAndView("admin/categories/add");
+			return new ModelAndView("admin/categories/AddOrEdit");
 		}
 		CategoryEntity entity = new CategoryEntity();
 
@@ -58,40 +69,88 @@ public class CategoryController {
 
 		String message = "";
 		if (cateModel.getIsEdit() == true) {
-			message = "Them thanh cong";
-		}
-		else
-		{
-			message="Them khong thanh cong";
+			message = "Chỉnh sửa thành công";
+		} else {
+			message = "Đã lưu thành công";
 		}
 
 		model.addAttribute("message", message);
 		return new ModelAndView("forward:/admin/categories", model);
-		
 	}
 	
-	@GetMapping("/edit/{id}")
-	public ModelAndView edit(ModelMap model, @PathVariable("id") Long categoryId) {
+	@GetMapping("edit/{categoryId}")
+	public ModelAndView edit(ModelMap model, @PathVariable("categoryId") Long categoryId) {
 		Optional<CategoryEntity> optCate = categoryService.findById(categoryId);
 		CategoryModel cateModel = new CategoryModel();
 		if (optCate.isPresent()) {
-			CategoryEntity entity = new CategoryEntity();
+			CategoryEntity entity = optCate.get();
 			
 			BeanUtils.copyProperties(entity, cateModel);
 			cateModel.setIsEdit(true);
-			model.addAttribute("cate", cateModel);
-			return new ModelAndView("admin/category/add", model);
+			model.addAttribute("category", cateModel);
+			return new ModelAndView("admin/categories/AddOrEdit", model);
 		}
 		model.addAttribute("message", "category is not existed");
 		return new ModelAndView("forward:/admin/categories", model);
 	}
 	
-	@GetMapping("delete/{id}")
-	public ModelAndView delete(ModelMap model, @PathVariable("id") Long categoryId) {
+	@GetMapping("delete/{categoryId}")
+	public ModelAndView delete(ModelMap model, @PathVariable("categoryId") Long categoryId) {
 		categoryService.deleteById(categoryId);
 		model.addAttribute("message","Deleted!");
 		return new ModelAndView("forward:/admin/categories", model);
 	}
 	
 	
-}
+	@GetMapping("search")
+	
+		public String search(ModelMap model, @RequestParam(name="name", required=false) String name) {
+			List<CategoryEntity> list=null;
+			if(StringUtils.hasText(name))
+			{
+				list=categoryService.findAll();
+			}
+			model.addAttribute("categories", list);
+			return "admin/categories/search";
+			
+			
+		}
+	
+	@RequestMapping("searchpaginated")
+	public String search1(ModelMap model,
+			@RequestParam(name="name", required = false) String name,
+			@RequestParam("page") Optional<Integer> page,
+			@RequestParam("size") Optional<Integer> size){
+		int count=(int) categoryService.count();
+		int currentPage=page.orElse(1);
+		int pagesize=size.orElse(3);
+		Pageable pageable=PageRequest.of(currentPage-1, pagesize,Sort.by("name"));
+		Page<CategoryEntity> resultPage=null;
+		if(StringUtils.hasText(name))
+		{
+			resultPage=categoryService.findByNameContaining(name, pageable);
+			model.addAttribute("name", name);
+		}
+		else
+		{
+			resultPage = categoryService.findAll(pageable);
+		}
+		int totalPages=resultPage.getTotalPages();
+		if(totalPages>0)
+		{
+			int start=Math.max(1, currentPage-2);
+			int end=Math.min(currentPage+2, totalPages);
+			if(totalPages> count)
+			{
+				if(end==totalPages) start=end-count;
+				else if(start==1) end=start+count;
+			}
+			List<Integer> pageNumbers=IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+			model.addAttribute("pageNumbers", pageNumbers);
+		}
+		model.addAttribute("categoryPage", resultPage);
+		
+	
+	return "admin/categories/searchpaginated";
+	}
+}	
--- src/main/java/vn/iotstar/thymeleaf/entity/CategoryEntity.java
+++ src/main/java/vn/iotstar/thymeleaf/entity/CategoryEntity.java
@@ -11,28 +11,28 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
-@Table(name= "categories")
-public class CategoryEntity implements Serializable {
-
+@Table(name="Category")
+public class CategoryEntity implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long categoryId;
-	@Column(name="category_name", length=200, columnDefinition ="nvarchar(200) not null")
-	private String name;
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	
+	@Column(name="categoryId")
+	private Long categoryid;
+	@Column(name="category_name", length=200,columnDefinition = "nvarchar(200) not null")
+	@NotEmpty(message="Khong dc rong")
+	private String name;
 	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
 	private Set<ProductEntity> products;
-
 }
